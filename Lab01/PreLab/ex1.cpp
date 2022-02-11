911ddf30f3769941120931ba381b0010c67afa68
baoivy <97684354+baoivy@users.noreply.github.com>
2022-02-11 10:19:21 +0700

    Update ex1.cpp

--- Lab01/PreLab/ex1.cpp
+++ Lab01/PreLab/ex1.cpp
@@ -1,1 +1,35 @@
+// Student may implement another function as need
+void Delete(char* str,int index)
+{
+	if (index == strlen(str) - 1)
+	{
+		str[index] = '\0';
+	}
+	for (int i = index; i < strlen(str); i++)
+	{
+		str[i] = str[i + 1];
+	}
+	str[strlen(str)] = '\0';
+}
 
+void process(char str[], char outstr[]) {
+    // TODO
+    for (int i = 0; i < strlen(str); i++)
+	{
+		outstr[i] = str[i];
+	}
+	outstr[strlen(str)] = '\0';
+	for (int i = 0; i < strlen(outstr); i++)
+	{
+		if (outstr[i] == ' ' && outstr[i + 1] == ' ')
+		{
+			Delete(outstr, i);
+			i--;
+		}
+	}
+
+	if (outstr[0] == ' ')
+		Delete(outstr, 0);
+	if (outstr[strlen(outstr) - 1] == ' ')
+		Delete(outstr, strlen(outstr) - 1);
+}
