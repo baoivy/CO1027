15f509f85acddd34b9141e9f71a9e1ed51f2b310
baoivy <97684354+baoivy@users.noreply.github.com>
2022-05-06 18:54:53 +0700

    Update sumofArr.cpp

--- Review/sumofArr.cpp
+++ Review/sumofArr.cpp
@@ -4,21 +4,22 @@ using namespace std;
 
 int countwaySumArray(vector <int> arr, int x, int idx = 0)
 {
+	if (idx >= arr.size())
+		return 0;
 	int res = arr[idx];
 	if (x == res)
 		return 1;
 	if (x < res)
 		return 0;
-	if (idx >= arr.size())
-		return 0;
-	int a = countwaySumArray(arr , x - res, idx + 1);
+
+	int a = countwaySumArray(arr, x - res, idx + 1);
 	int b = countwaySumArray(arr, x, idx + 1);
 	return a + b;
 }
 
 int main()
 {
-	vector <int> arr{ 1,2,1,3,4};
+	vector <int> arr{ 1,2,1,3,4,5,6,7,8,9,10,11,12};
 	int x = 4;
 	cout << countwaySumArray(arr, x);
 	return 0;
